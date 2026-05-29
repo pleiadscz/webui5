693771e5ff069948d8e3ef6108de8aff6ff9512b
pleiadscz <pleiads@seznam.cz>
2026-05-29 21:21:37 +0200

    Initial commit

---
+++ src/lib/components/chat/ChatPlaceholder.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import { WEBUI_API_BASE_URL, WEBUI_BASE_URL } from '$lib/constants';
+	import { marked } from 'marked';
+	import DOMPurify from 'dompurify';
+
+	import { config, user, models as _models, temporaryChatEnabled } from '$lib/stores';
+	import { onMount, getContext } from 'svelte';
+
+	import { blur, fade } from 'svelte/transition';
+
+	import Suggestions from './Suggestions.svelte';
+	import { sanitizeResponseContent } from '$lib/utils';
+	import Tooltip from '$lib/components/common/Tooltip.svelte';
+	import EyeSlash from '$lib/components/icons/EyeSlash.svelte';
+
+	const i18n = getContext('i18n');
+
+	export let modelIds = [];
+	export let models = [];
+	export let atSelectedModel;
+
+	export let onSelect = (e) => {};
+
+	let mounted = false;
+	let selectedModelIdx = 0;
+
+	$: if (modelIds.length > 0) {
+		selectedModelIdx = models.length - 1;
+	}
+
+	$: models = modelIds.map((id) => $_models.find((m) => m.id === id));
+
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+{#key mounted}
+	<div class="m-auto w-full max-w-6xl px-8 lg:px-20">
+		<div class="flex justify-start">
+			<div class="flex -space-x-4 mb-0.5" in:fade={{ duration: 200 }}>
+				{#each models as model, modelIdx}
+					<button
+						on:click={() => {
+							selectedModelIdx = modelIdx;
+						}}
+					>
+						<Tooltip
+							content={DOMPurify.sanitize(
+								marked.parse(
+									sanitizeResponseContent(
+										models[selectedModelIdx]?.info?.meta?.description ?? ''
+									).replaceAll('\n', '<br>')
+								)
+							)}
+							placement="right"
+						>
+							<img
+								src={`${WEBUI_API_BASE_URL}/models/model/profile/image?id=${model?.id}&lang=${$i18n.language}`}
+								class=" size-[2.7rem] rounded-full border-[1px] border-gray-100 dark:border-none"
+								alt="logo"
+								draggable="false"
+								on:error={(e) => {
+									e.currentTarget.src = '/favicon.png';
+								}}
+							/>
+						</Tooltip>
+					</button>
+				{/each}
+			</div>
+		</div>
+
+		{#if $temporaryChatEnabled}
+			<Tooltip
+				content={$i18n.t("This chat won't appear in history and your messages will not be saved.")}
+				className="w-full flex justify-start mb-0.5"
+				placement="top"
+			>
+				<div class="flex items-center gap-2 text-gray-500 text-lg mt-2 w-fit">
+					<EyeSlash strokeWidth="2.5" className="size-5" />{$i18n.t('Temporary Chat')}
+				</div>
+			</Tooltip>
+		{/if}
+
+		<div
+			class=" mt-2 mb-4 text-3xl text-gray-800 dark:text-gray-100 text-left flex items-center gap-4 font-primary"
+		>
+			<div>
+					<div class=" capitalize line-clamp-1" in:fade={{ duration: 200 }}>
+						{$i18n.t('Jak mogę ci pomóc?')}
+					</div>
+
+					<div in:fade={{ duration: 200, delay: 200 }}>
+					</div>
+			</div>
+		</div>
+
+		<div class=" w-full font-primary" in:fade={{ duration: 200, delay: 300 }}>
+			<Suggestions
+				className="grid grid-cols-2"
+				suggestionPrompts={atSelectedModel?.info?.meta?.suggestion_prompts ??
+					models[selectedModelIdx]?.info?.meta?.suggestion_prompts ??
+					$config?.default_prompt_suggestions ??
+					[]}
+				{onSelect}
+			/>
+		</div>
+	</div>
+{/key}
